4d90b036596677045aa4f17f6f6ef05376f0d421
Stephen Wolf <199786wolf@chsbr.net>
2019-01-09 09:10:47 -0600

    Streamlined imports
    - Added import all for commands and subsystems so we don't have to add each

--- Command-Based/src/main/java/frc/robot/Robot.java
+++ Command-Based/src/main/java/frc/robot/Robot.java
@@ -12,10 +12,8 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.Drive;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.MecanumSubsystem;
+import frc.robot.commands.*;
+import frc.robot.subsystems.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the
