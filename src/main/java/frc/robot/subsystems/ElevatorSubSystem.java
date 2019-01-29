8d100ac3e8d8793d78c276f7795f92c864bf54b4
josh Harold <202885harold@chsbr.net>
2019-01-29 17:51:41 -0600

    josh harold 1/29/2019
    started working on more complex elevator

--- src/main/java/frc/robot/subsystems/ElevatorSubSystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubSystem.java
@@ -11,6 +11,8 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 import javax.lang.model.util.ElementScanner6;
 
+import com.sun.tools.sjavac.Log.Level;
+
 //import robotmap
 import frc.robot.RobotMap;
 //dashboard 
@@ -21,6 +23,19 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class ElevatorSubSystem extends Subsystem 
 {
+  //level names
+  String CurrentLevelName = "";
+  String SetLevelName = "";
+
+  //make a storage varable for the mode
+  String mode = "PanelMode";
+
+  //make a varable to hold the direction the Dpad is being pressed
+  String DPad = "none";
+
+  //make a bool for manual
+  boolean Manual = false;
+
   //controller values
   boolean Pressed = false;
 
@@ -44,28 +59,163 @@ public class ElevatorSubSystem extends Subsystem
   
   public void Teleop()
   {
-    //controller
-    if(Map.controllerTwo.LeftYAxis() < -0.2)     //check if its being pressed up
+    //check for a switch in modes 
+    if(Map.controllerTwo.Y_Button())
     {
-      //check to see if the controller is already pressed
       if(!Pressed)
       {
-        //if not incrament the set value
-        SetLevel++;
+        mode = "PanelMode";
+        Manual = false;
       }
       Pressed = true;
-    }else if(Map.controllerTwo.LeftYAxis() > 0.2)     //check if its being pressed downn
+    }else if(Map.controllerTwo.B_Button())
     {
-      //check to see if the controller is already pressed
       if(!Pressed)
       {
-        //if not deincrament the set value
-        SetLevel--;
+        mode = "BallMode";
+        Manual = false;
       }
       Pressed = true;
-    }else{//nothing is being pressed that we care about so change pressed to false
+      
+    }else if(Map.controllerTwo.X_Button())
+    {
+      if(!Pressed)
+      {
+        mode = "CargoShip";
+        Manual = false;
+      }
+      Pressed = true;
+      
+    }else if(Map.controllerTwo.A_Button())
+    {
+      if(!Pressed)
+      {
+        mode = "Reserved";
+        Manual = false;
+      }
+      Pressed = true;
+      
+    }else if(Map.controllerTwo.LeftYAxis() < -0.2)     //check if its being pressed up
+    {
+      if(!Pressed)
+      {
+        DPad = "Up";
+        Manual = false;
+      }
+      Pressed = true;
+
+    }else if(Map.controllerTwo.LeftYAxis() > 0.2)     //check if its being pressed down
+    {
+      if(!Pressed)
+      {
+        DPad = "Down";
+        Manual = false;
+      }
+      Pressed = true;
+      
+    }else if(Map.controllerTwo.LeftXAxis() > 0.2)     //check if its being pressed left
+    {
+      if(!Pressed)
+      {
+        DPad = "Left";
+        Manual = false;
+      }
+      Pressed = true;
+      
+    }else if(Map.controllerTwo.LeftXAxis() < -0.2)     //check if its being pressed right
+    {
+      if(!Pressed)
+      {
+        DPad = "Right";
+        Manual = false;
+      }
+      Pressed = true;
+      
+    }else{
+      //set the direction to none 
+      DPad = "none";
+
+      //set pressed to false
       Pressed = false;
     }
+
+    //display the current mode
+    SmartDashboard.putString("Current Elevador Mode", mode);
+
+    //TODO get actual level numbers
+    //do what ya need to do for the mode
+    switch(mode)
+    {
+      case "PanelMode":
+      switch(DPad)
+      {
+        case "Left":
+        SetLevel = 2;
+        break;
+        case "Up":
+        SetLevel = 3;
+        break;
+        case "Right":
+        SetLevel = 4;
+        break;
+        case "Down":
+        SetLevel = 1;
+        break;
+      }
+      break;
+      case "BallMode":
+      switch(DPad)
+      {
+        case "Left":
+        SetLevel = 3;
+        break;
+        case "Up":
+        SetLevel = 6;
+        break;
+        case "Right":
+        SetLevel = 7;
+        break;
+        case "Down":
+        SetLevel = 0;
+        break;
+      }
+      break;
+      case "CargoShip":
+      switch(DPad)
+      {
+        case "Left":
+        SetLevel = 2;
+        break;
+        case "Up":
+        SetLevel = 0;
+        break;
+        case "Right":
+        SetLevel = 2;
+        break;
+        case "Down":
+        SetLevel = 1;
+        break;
+      }
+      break;
+      case "Reserved":
+      switch(DPad)
+      {
+        case "Left":
+        SetLevel = 0;
+        break;
+        case "Up":
+        SetLevel = 0;
+        break;
+        case "Right":
+        SetLevel = 0;
+        break;
+        case "Down":
+        SetLevel = 0;
+        break;
+      }
+      break;
+    }
+
     if(SetLevel < 0)
     {
       SetLevel = 0;
@@ -82,24 +232,82 @@ public class ElevatorSubSystem extends Subsystem
     if(CurrentLevel < SetLevel)
     {
       Map.Elevator.set(0.2);
-      SmartDashboard.putBoolean("DB/LED 0", false);
+      SmartDashboard.putBoolean("Is current level = to set level", false);
     }
     //is it above
     if(CurrentLevel > SetLevel)
     {
       Map.Elevator.set(-0.2);
-      SmartDashboard.putBoolean("DB/LED 0", false);
+      SmartDashboard.putBoolean("Is current level = to set level", false);
     }
     //is it at the set level
     if(CurrentLevel == SetLevel)
     {
       Map.Elevator.set(0.0);
-      SmartDashboard.putBoolean("DB/LED 0", true);
+      SmartDashboard.putBoolean("Is current level = to set level", true);
     } 
 
     //display values
-    SmartDashboard.putString("CurrentLevel", Integer.toString(CurrentLevel));
-    SmartDashboard.putString("SetLevel", Integer.toString(SetLevel));
+
+    //display the level name
+    switch(CurrentLevel)
+    {
+      case 0:
+        CurrentLevelName = "Level 1";
+      break;
+      case 1:
+      CurrentLevelName = "Level 2";
+      break;
+      case 2:
+      CurrentLevelName = "Level 3";
+      break;
+      case 3:
+      CurrentLevelName = "Level 4";
+      break;
+      case 4:
+      CurrentLevelName = "Level 5";
+      break;
+      case 5:
+      CurrentLevelName = "Level 6";
+      break;
+      case 6:
+      CurrentLevelName = "Level 7";
+      break;
+      case 7:
+      CurrentLevelName = "Level 8";
+      break;
+    }
+    SmartDashboard.putString("CurrentLevel", CurrentLevelName);
+
+    //display the level its set to
+    switch(CurrentLevel)
+    {
+      case 0:
+      SetLevelName = "Level 1";
+      break;
+      case 1:
+      SetLevelName = "Level 2";
+      break;
+      case 2:
+      SetLevelName = "Level 3";
+      break;
+      case 3:
+      SetLevelName = "Level 4";
+      break;
+      case 4:
+      SetLevelName = "Level 5";
+      break;
+      case 5:
+      SetLevelName = "Level 6";
+      break;
+      case 6:
+      SetLevelName = "Level 7";
+      break;
+      case 7:
+      SetLevelName = "Level 8";
+      break;
+    }
+    SmartDashboard.putString("SetLevel", SetLevelName);
 
   }
 
